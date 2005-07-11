81abb9caf041ca28252750e226bada7eb85339bb
Mike <Mike@cbf1734e-0d0c-7a49-982f-03f86ddce502>
2005-07-11 04:25:43 +0000

    new class structure

--- index.php
+++ index.php
@@ -15,72 +15,8 @@ define('rootpath', './');
 // which errors to report?
 error_reporting(E_ALL);
 
-// core class to store globally accessible stuff
-class ib_core {
-	var $version = '0.1 dev';
-	
-	var $conf;
-	
-	var $db;
-	var $func;
-	var $skin;
-	var $output;
-	
-	var $member;
-	
-	var $timer;
-	
-	function ib_core($conf_file) {
-		require $conf_file;
-		
-		$this->conf =& $conf;
-		
-		$this->member = array('m_id' => 0);
-	}
-	
-	function finish() {
-		//won't be needing this no more
-		$this->db->close();
-		
-		// get the server load for stats
-		$load = '-';
-		if(strpos(strtolower(PHP_OS), "win") === false) {
-			if(file_exists("/proc/loadavg")) {
-				$load = file_get_contents("/proc/loadavg");
-				$load = explode(' ', $load);
-				$load = $load[0];
-			}
-		}
-		
-		// how long did we take?
-		$stats = $this->skin->stats(array('exec' => $this->timer->stop(), 'sql' => $this->db->times['total'], 'queries' => $this->db->query_count, 'load' => $load, 'gzip' => 'Off'));
-		
-		return $stats;
-	}
-}
-
-// timer class
-class timer {
-	var $start;
-	var $end;
-	var $total;
-	
-	function start() {
-		$micro = microtime();
-		$micro = explode(' ', $micro);
-		$this->start = $micro[1] + $micro[0];
-	}
-	
-	function stop($precision = 4) {
-		$micro = microtime();
-		$micro = explode(' ', $micro);
-		$this->end = $micro[1] + $micro[0];
-		
-		$this->total = $this->end - $this->start;
-		
-		return round($this->total, $precision);
-	}
-}
+// define the core class
+require_once rootpath.'sources/lib/core.php';
 
 // setup our core class so we can get onto other things
 $ib_core = new ib_core(rootpath.'ib_config.php');
@@ -89,12 +25,8 @@ $ib_core = new ib_core(rootpath.'ib_config.php');
 $ib_core->timer = new timer();
 $ib_core->timer->start();
 
-// get our standard function library before anything else
-require rootpath.'sources/lib/functions.php';
-$ib_core->func = new functions($ib_core);
-
 // setup the database
-require rootpath.'sources/lib/db_'.$ib_core->conf['db_driver'].'.php';
+require_once rootpath.'sources/lib/db_'.$ib_core->conf['db_driver'].'.php';
 $ib_core->db = new database($ib_core);
 
 // get ready for output
@@ -102,10 +34,10 @@ require rootpath.'sources/lib/output.php';
 $ib_core->output = new output($ib_core);
 
 // get the global skin ready
-$ib_core->skin =& $ib_core->func->load_skin('global');
+$ib_core->load_skin('global');
 
 // retrieve user input
-$ib_core->input = $ib_core->func->get_input();
+$ib_core->input = $ib_core->get_input();
 
 // connect the database
 $ib_core->db->connect();
@@ -127,7 +59,7 @@ elseif(!isset($acts[$ib_core->input['act']])) {
 }
 
 // do it
-require rootpath."sources/{$acts[$ib_core->input['act']][1]}.php";
+require_once rootpath."sources/{$acts[$ib_core->input['act']][1]}.php";
 $act = new $acts[$ib_core->input['act']][0]($ib_core);
 
 
